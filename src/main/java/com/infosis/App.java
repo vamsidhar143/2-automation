19b6742f5893d6f120020fae48fa0b3579c57e93
root <root@ip-172-31-1-248.ap-south-1.compute.internal>
2022-12-09 13:03:58 +0000

    added files

---
+++ src/main/java/com/infosis/App.java
@@ -0,0 +1,13 @@
+package com.infosis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
